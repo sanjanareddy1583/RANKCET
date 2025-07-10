90ac17a6ca7e3ed019f1e3dfd84f5b9d662a5cc7
sanjanareddy1583 <sanjanareddy1583@gmail.com>
2025-07-10 21:10:54 +0530

    🔥 Fixed data.js default export

--- src/data.js
+++ src/data.js
@@ -1,7 +1,9 @@
-export const colleges = [
+const colleges = [
   { name: "JNTUH", rankRange: [1, 10000], branch: "CSE" },
   { name: "OU", rankRange: [5000, 15000], branch: "ECE" },
   { name: "CVR", rankRange: [10000, 25000], branch: "IT" },
   { name: "VNR VJIET", rankRange: [12000, 30000], branch: "CSE" },
   { name: "Malla Reddy", rankRange: [25000, 50000], branch: "EEE" },
 ];
+
+export default colleges;
