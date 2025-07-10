86b0ad2b3a3cedf0ff6710c46ec73d82ac1614b4
sanjanareddy1583 <sanjanareddy1583@gmail.com>
2025-07-10 21:22:51 +0530

    Updated App.js: centered results and added Enter key

--- src/data.js
+++ src/data.js
@@ -1,19 +1,10 @@
 const data = [
-  {
-    name: "JNTU Hyderabad",
-    branch: "CSE",
-    closingRank: 1000
-  },
-  {
-    name: "OU Hyderabad",
-    branch: "ECE",
-    closingRank: 1500
-  },
-  {
-    name: "CBIT",
-    branch: "IT",
-    closingRank: 2500
-  }
+  { name: 'JNTU Hyderabad', branch: 'CSE', closingRank: 3000 },
+  { name: 'OU Hyderabad', branch: 'ECE', closingRank: 5000 },
+  { name: 'CBIT', branch: 'IT', closingRank: 8000 },
+  { name: 'MGIT', branch: 'CSE', closingRank: 10000 },
+  { name: 'Vasavi', branch: 'ECE', closingRank: 6000 },
+  { name: 'MVSR', branch: 'EEE', closingRank: 12000 }
 ];
 
 export default data;
